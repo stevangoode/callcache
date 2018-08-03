fee4b6d3a0db0a4004bd60941eabf62fd747b692
Stevan Goode <stevan.goode@skybettingandgaming.com>
2018-08-03 16:44:16 +0100

    Updated c variable name, added more examples

--- callcache.go
+++ callcache.go
@@ -4,19 +4,33 @@
 //
 // Simple Example:
 //
-// c := callcache.CallCache{
+// cache := callcache.CallCache{
 //   Interval: time.Duration(2 * time.Second)
 //   Call: api.GetMyData
 // }
-// c.Start()
+// cache.Start()
 //
 // With Parameters:
 //
-// c := callcache.CallCache{
+// cache := callcache.CallCache{
 //   Interval: time.Duration(2 * time.Second)
 //   Call: func () []api.MyDataType {return api.GetMyData(aParameter)}
 // }
-// c.Start()
+// cache.Start()
+//
+// Fetching Data:
+//
+// myData := cache.Fetch()
+//
+// Stop Polling:
+//
+// cache.Stop()
+//
+// Change Duration:
+//
+// cache.Stop()
+// cache.Interval = time.Duration(10 * time.Second)
+// cache.Start()
 package callcache
 
 import (
@@ -34,50 +48,50 @@ type CallCache struct {
 }
 
 // Fetch gets the cached function response
-func (c *CallCache) Fetch() interface{} {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
+func (cache *CallCache) Fetch() interface{} {
+	cache.lock.RLock()
+	defer cache.lock.RUnlock()
 
-	return c.cache
+	return cache.cache
 }
 
 // Start begins the polling. Returns a channel which, when closed, will stop the polling
-func (c *CallCache) Start() chan interface{} {
-	c.stop = make(chan interface{})
+func (cache *CallCache) Start() chan interface{} {
+	cache.stop = make(chan interface{})
 
 	// Immediately start fetching the results of the method
-	c.update()
+	cache.update()
 
 	// This func will cause the cache to be updated on a schedule
 	go func() {
-		timer := time.NewTicker(c.Interval * time.Millisecond)
+		timer := time.NewTicker(cache.Interval * time.Millisecond)
 
 		for {
 			select {
 			case <-timer.C:
-				c.update()
+				cache.update()
 
-			case <-c.stop:
+			case <-cache.stop:
 				timer.Stop()
 				return
 			}
 		}
 	}()
 
-	return c.stop
+	return cache.stop
 }
 
 // Stop will stop the polling and updating until Start is called again
-func (c *CallCache) Stop() {
-	close(c.stop)
-	c.stop = nil
+func (cache *CallCache) Stop() {
+	close(cache.stop)
+	cache.stop = nil
 }
 
 // Update causes the call to be done, and the result cached
-func (c *CallCache) update() {
-	response := c.Call()
+func (cache *CallCache) update() {
+	response := cache.Call()
 
-	c.lock.Lock()
-	c.cache = response
-	c.lock.Unlock()
+	cache.lock.Lock()
+	cache.cache = response
+	cache.lock.Unlock()
 }
